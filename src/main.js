c2caed3156fb6527a0056cd99ba2efb853e4a03c
chamherz <adrielchambers@gmail.com>
2020-07-23 20:11:19 -0300

    reacto funcando

--- src/main.js
+++ src/main.js
@@ -2,21 +2,43 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+const isDev = require('electron-is-dev');
+let mainWindow;
+
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1000,
+    title: "AudioLab"
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(
+      isDev
+          ? "http://localhost:3000"
+          : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 
+  if (isDev) {
+    // Using require
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+    } = require("electron-devtools-installer");
+
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => {
+          console.log(`Added Extension:  ${name}`);
+        })
+        .catch((err) => {
+          console.log("Error ocurrido: ", err);
+        });
+  }
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.once('dom-ready', () => {
+    mainWindow.webContents.openDevTools();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -28,7 +50,9 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   })
 })
 
@@ -36,6 +60,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
+  console.log("Saliendo")
   if (process.platform !== 'darwin') app.quit()
 })
 
